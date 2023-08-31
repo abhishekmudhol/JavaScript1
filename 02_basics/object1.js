511f4f79369188b1c1351f8cbeac7c1415db55a2
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-09-01 01:56:00 +0530

    object1 completed

--- 02_basics/object1.js
+++ 02_basics/object1.js
@@ -2,7 +2,7 @@
 /*  
  * object can be declare in two ways
  *  1. as literals
- * 2. as constructor
+ *  2. as constructor
  * 
  *   // singleton -> when we create object with constructor then
  *                   singleton object gets created
@@ -58,13 +58,88 @@
         console.log(user_1.id);
 
 
+        const  webUser = {
 
+            name : "ABHISHEK",
+            "database Name" : "mondoDB" , // space between database and Name
+            age : 21,
+            email : "abhishekmudholatwork.com", 
+            lastLoggedIn : ["monday","sunday"],
+            object1 :  {}
+        }
+/****************** 2 WAYS OF ACCESS OBJETS ***************/
 
+     console.log(webUser.email);   // dot notation
+     console.log(webUser["email"]); // squarebraket notation 
 
+     // we cannot access database Name key using dot . we need [] squarebraket notation 
+     
+     // console.log(webUser.database Name);  -> not possible
+        console.log(webUser["database Name"]);  //mongoDB
 
+/**********************************************************/
 
+  //           USING SYMBOL AS OBJECT KEY
 
+  /**
+   *  if you write symbol without [ ] in object declaration 
+   * it show it as string i.e. typeof is string 
+   * 
+   */
 
+const mysymbol = Symbol("key1") ;
 
+const  webUser1 = {
 
+    [mysymbol] : "myUniqueKeyOne" ,  
         
+    name : "ABHISHEK",
+    "database Name" : "mondoDB" , // space between database and Name
+    age : 21,
+    email : "abhishekmudholatwork.com", 
+    lastLoggedIn : ["monday","sunday"],
+    object1 :  {}
+}
+           
+    //  console.log(webUser1.mysymbol); // myUniqueKeyOne but type is string not symbol
+     
+    //  console.log(typeof webUser1.mysymbol); // string
+     
+    console.log(webUser1[mysymbol]); // accessed like this without " "
+    
+    console.log(webUser1) //[Symbol(key1)]: 'myUniqueKeyOne'// see type is symbole there
+/**********************************************************/
+   
+   //              MODIFYING VALUES IN OBJECT
+
+
+     webUser1.email = "abhishekmudholatwork@gmail.com"
+
+console.log(webUser1.email); //abhishekmudholatwork@gmail.com
+
+/**********************************************************/
+
+//            LOCKING VALUES OF OBJECT
+
+   // Object.freeze(webUser1);  // commented because show error 
+
+webUser1.age = 22 ;    
+console.log(webUser1.age); // 21 not changed
+
+/**********************************************************/
+
+//            ADDING FUNCTION IN OBJECT
+
+ webUser1.greeting = function() { console.log("Hello webUser1");}
+    
+           console.log(webUser1.greeting); // [Function (anonymous)]
+           console.log(webUser1.greeting()); // Hello webUser1
+
+        /**  this -> to refernce same object  **/
+
+ webUser1.greetingWithUserName = function(){console.log(`Hello ${this.name}`);}          
+
+           console.log(webUser1.greetingWithUserName); // [Function (anonymous)]
+           console.log(webUser1.greetingWithUserName()); // Hello ABHISHEK
+
+/**********************************************************/           
