9c8ecf6e5dec820f04e9874df1f418cd9c2869fc
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-09-07 21:51:57 +0530

    04_basic create

---
+++ 04_basics/arrowFunc.js
@@ -0,0 +1,13 @@
+/*************************THIS KEYWORD************************/
+const user ={
+
+    username : "abhishek",
+    price : 1000,
+
+    welcomeMessage : function () {
+        console.log(`${this.username}, welcome to website`);
+    } 
+}
+
+
+user.welcomeMessage()
