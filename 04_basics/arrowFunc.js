dc93b3b72b3cedb31fa4f76622a7f89d927e6050
abhishekmudhol <104548603+abhishekmudhol@users.noreply.github.com>
2023-09-08 22:38:42 +0530

    arrow function

--- 04_basics/arrowFunc.js
+++ 04_basics/arrowFunc.js
@@ -1,5 +1,5 @@
 /*************************THIS KEYWORD************************/
-const user ={
+const user1 ={
 
     username : "abhishek",
     price : 1000,
@@ -11,3 +11,58 @@ const user ={
 
 
 user.welcomeMessage()
+
+const user = {
+    username: "hitesh",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function () {
+//     let username = "hitesh"
+//     console.log(this.username);
+// }
+
+const chai =  () => {
+    let username = "hitesh"
+    console.log(this);
+}
+
+
+// chai()
+
+// const addTwo = (num1, num2) => {
+//     return num1 + num2
+// }
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ({username: "hitesh"})
+
+
+console.log(addTwo(3, 4))
+
+
+// const myArray = [2, 5, 3, 7, 8]
+
+// myArray.forEach()
